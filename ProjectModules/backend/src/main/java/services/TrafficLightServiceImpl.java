8679ddc6cbe4d116d5cf085e27bdad4ccdaaae34
otttisss <otttisss@yandex.ru>
2023-10-15 12:29:27 +0300

    TrafficLightServiceImpl created

---
+++ ProjectModules/backend/src/main/java/services/TrafficLightServiceImpl.java
@@ -0,0 +1,25 @@
+package services;
+
+import interfaces.TrafficLight;
+import interfaces.TrafficLightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import interfaces.TrafficLightsService;
+
+@Service
+public class TrafficLightServiceImpl implements TrafficLightsService{
+    @Autowired
+    private TrafficLightRepository trafficLightRepository;
+    @Autowired
+    private TrafficLight trafficLight;
+
+    @Override
+    public void setMode(String mode) {
+        trafficLightRepository.saveState(mode);
+    }
+
+    @Override
+    public String getCurrentMode() {
+        return trafficLightRepository.getCurrentState();
+    }
+}
