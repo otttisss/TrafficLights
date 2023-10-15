4953b8c3b0eda106b8079c6adf33246696c7debd
otttisss <otttisss@yandex.ru>
2023-10-15 16:16:03 +0300

    TrafficLightControllerTest added

---
+++ ProjectModules/logic-rest/src/test/java/tests/TrafficLightControllerTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import interfaces.TrafficLightsService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TrafficLightControllerTest.class)
+public class TrafficLightControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private TrafficLightsService trafficLightsService;
+
+    @Test
+    public void testSwitchToRed() throws Exception {
+        mockMvc.perform(post("/switch-red")).andExpect(status().isOk());
+    }
+}
