f549e84cbbd61534fbf9d4e2d709284937285f17
otttisss <otttisss@yandex.ru>
2023-10-15 17:38:52 +0300

    fixed

--- ProjectModules/logic-rest/src/test/java/com/example/trafficlight/TrafficLightControllerTest.java
+++ ProjectModules/logic-rest/src/test/java/com/example/trafficlight/TrafficLightControllerTest.java
@@ -1,10 +1,13 @@
 package com.example.trafficlight;
 
+import com.example.trafficlight.controllers.TrafficLightController;
 import com.example.trafficlight.interfaces.TrafficLightsService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,7 +16,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(TrafficLightControllerTest.class)
+//@WebMvcTest(TrafficLightControllerTest.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TrafficLightControllerTest {
     @Autowired
     private MockMvc mockMvc;
