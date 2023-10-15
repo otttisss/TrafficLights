9da8ab463fd81947aa31c63c3d57df84048adf17
otttisss <otttisss@yandex.ru>
2023-10-15 11:51:38 +0300

    exception created

--- ProjectModules/interfaces/src/main/java/exceptions/TrafficLightException.java
+++ ProjectModules/interfaces/src/main/java/exceptions/TrafficLightException.java
@@ -1,4 +1,7 @@
 package exceptions;
 
-public class TrafficLightException {
+public class TrafficLightException extends RuntimeException {
+    public TrafficLightException(String message) {
+        super(message);
+    }
 }
