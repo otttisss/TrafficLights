8394bd1c8c8b30993d34d3db7c9a4bb04747dab5
otttisss <otttisss@yandex.ru>
2023-10-15 12:22:03 +0300

    controller added

---
+++ ProjectModules/logic-rest/src/main/java/controllers/TrafficLightController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import interfaces.TrafficLight;
+import interfaces.TrafficLightsService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+@RestController
+public class TrafficLightController {
+    @Autowired
+    private TrafficLight trafficLight;
+    @Autowired
+    private TrafficLightsService trafficLightsService;
+
+    @GetMapping("/current-state")
+    public String getCurrentState(){
+        return trafficLightsService.getCurrentMode();
+    }
+
+    @PostMapping("/set-mode")
+    public void setMode(@RequestParam String mode) {
+        trafficLightsService.setMode(mode);
+    }
+
+    @PostMapping("/switch-red")
+    public void switchToRed() {
+        trafficLight.switchToRed();
+    }
+
+    @PostMapping("/switch-yellow")
+    public void switchToYellow() {
+        trafficLight.switchToYellow();
+    }
+
+    @PostMapping("/switch-green")
+    public void switchToGreen() {
+        trafficLight.switchToGreen();
+    }
+}
